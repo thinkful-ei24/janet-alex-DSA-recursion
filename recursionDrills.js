1d67e87bf5e738f2ddfbf78fcf7f35d8f187be99
Janet Leon <leon.irenejanet@gmail.com>
2018-11-05 08:41:05 -0800

    added first question

---
+++ recursionDrills.js
@@ -0,0 +1,14 @@
+'use strict';
+/*
+Counting Sheep
+Write a recursive function that counts how many sheep jumps over the fence. 
+Your program should take a number as an input. That number should be the number of sheep you have. 
+The function should display the number along with the message "Another sheep jump over the fence" 
+until no more sheep are left.
+
+Input: 3
+Output:
+3 - Another sheep jump over the fence
+2 - Another sheep jump over the fence
+1 - Another sheep jump over the fence
+*/
